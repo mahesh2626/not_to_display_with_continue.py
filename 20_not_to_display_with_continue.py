d5b7dcb4ccb9c7301bbdf613d405ea7fbc90847a
mahesh2626 <118418933+mahesh2626@users.noreply.github.com>
2023-01-20 22:16:30 +0530

    Add files via upload

---
+++ 20_not_to_display_with_continue.py
@@ -0,0 +1,42 @@
+# n=0
+# while n<10:
+#     n+=1
+#     if n==6:
+#         continue
+#     print(n)
+
+
+""" 2nd type"""
+"""
+n=0
+while n<10:
+    n+=1
+    if n<5:
+        continue
+    print(n)
+"""
+
+""" 3rd type """
+"""
+n=0
+while n<10:
+    n+=1
+    if n>5:
+        continue
+    print(n)
+"""
+
+""" 4th type  """
+"""
+n=0
+while n<10:
+    n+=1
+    if n<=5:
+        continue
+    print(n)
+"""
+
+# if (11>2):
+#     pass
+# else:
+#     print('False')
